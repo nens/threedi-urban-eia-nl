dbb6095d806f99f91a182e8afb876c7f83ceb5ca
woutlexmond <47450870+woutlexmond@users.noreply.github.com>
2020-03-24 12:49:54 +0100

    Added input and output folders to project + relative paths for mock test

--- batch_calculator/tests/test_scripts.py
+++ batch_calculator/tests/test_scripts.py
@@ -5,15 +5,15 @@ import mock
 
 from batch_calculator import scripts
 
-# Relative path van maken
+# Arguments required by the parser
+rain_files_dir = os.path.relpath("tests/reeks_10jr_1955_test")
+org_id = "61f5a464c35044c19bc7d4b42d7f58cb"
+results_dir = os.path.relpath("tests/output")
+
+
 @mock.patch(
     "sys.argv",
-    [
-        "program",
-        "C:/Users/Wout.Lexmond/notebooks/reeksberekeningen/reeks_10jr_1955_test",
-        "61f5a464c35044c19bc7d4b42d7f58cb",
-        "C:/Users/Wout.Lexmond/notebooks/reeksberekeningen/output",
-    ],
+    ["program", rain_files_dir, "61f5a464c35044c19bc7d4b42d7f58cb", results_dir,],
 )
 def test_get_parser():
     parser = scripts.get_parser()
