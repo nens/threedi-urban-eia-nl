1a8509ba4b539b708b7ca1397329d17a08a1fae1
woutlexmond <47450870+woutlexmond@users.noreply.github.com>
2020-07-06 15:57:34 +0200

    Update scripts.py
    Added metavar and help to each parser argument

--- batch_calculator/scripts.py
+++ batch_calculator/scripts.py
@@ -135,11 +135,31 @@ def get_parser():
         default="61f5a464c35044c19bc7d4b42d7f58cb",
         help="UUID of the organisation used for the 3Di calculation",
     )
-    parser.add_argument("model_id")
-    parser.add_argument("rain_files_dir")
-    parser.add_argument("ini_2d_water_level")
-    parser.add_argument("results_dir")
-    parser.add_argument("--saved_state_url")
+    parser.add_argument(
+        "model_id",
+        metavar="MODEL_ID",
+        help="id number of the model as listed on: https://api.3di.live/v3.0/threedimodels/",
+    )
+    parser.add_argument(
+        "rain_files_dir",
+        metavar="RAIN_FILES_DIR",
+        help="The directory in which all the rain files are located",
+    )
+    parser.add_argument(
+        "ini_2d_water_level",
+        metavar="INI_2D_WATER_LEVEL",
+        help="The initial 2D water level in mNAP",
+    )
+    parser.add_argument(
+        "results_dir",
+        metavar="RESULTS_DIR",
+        help="The directory in which all downloaded results will be stored",
+    )
+    parser.add_argument(
+        "--saved_state_url",
+        metavar="SAVED_STATE_URL",
+        help="The url of the saved state that will be used at the start of the simulation. Saved states can be found here: https://api.3di.live/v3.0/files/",
+    )
     parser.add_argument(
         "nr_years",
         default="10",
