9a39eaa1ab0e99798306fd7f96d321b5dcc332e0
woutlexmond <47450870+woutlexmond@users.noreply.github.com>
2020-09-28 14:50:02 +0200

    Update AddDWF.py
    Removed DWF dictionary, because we use a DWF list

--- batch_calculator/AddDWF.py
+++ batch_calculator/AddDWF.py
@@ -30,15 +30,6 @@ dwfPerNode = []
 for row in c.execute(
     "WITH inhibs_per_node AS (SELECT impsurf.id, impsurf.nr_of_inhabitants, impmap.connection_node_id FROM v2_impervious_surface impsurf, v2_impervious_surface_map impmap WHERE impsurf.nr_of_inhabitants IS NOT NULL AND impsurf.nr_of_inhabitants != 0 AND impsurf.id = impmap.impervious_surface_id) SELECT ipn.connection_node_id, SUM(ipn.nr_of_inhabitants) FROM inhibs_per_node ipn GROUP BY connection_node_id"
 ):
-    # data["dwfNode"].append(
-    #     {
-    #         "offset": 0, # Wat doet offset bij laterals?
-    #         "values": [[0, 10], [20, 30]], # multiply second value with nr_of_inhabitants (row[1])
-    #         "units": "m3/s",
-    #         "connection_node": row[0],
-    #     }
-    # )
-    # print(row)
     dwfPerNode.append([row[0], row[1] * dwfPerPerson])
 
 # print(dwfPerNode)
@@ -46,33 +37,7 @@ for row in c.execute(
 # Close connection with spatialite
 conn.close()
 
-dwfDict = {
-    "0-1": 0.015,
-    "1-2": 0.015,
-    "2-3": 0.015,
-    "3-4": 0.015,
-    "4-5": 0.015,
-    "5-6": 0.03,
-    "6-7": 0.04,
-    "7-8": 0.05,
-    "8-9": 0.06,
-    "9-10": 0.065,
-    "10-11": 0.075,
-    "11-12": 0.085,
-    "12-13": 0.075,
-    "13-14": 0.065,
-    "14-15": 0.06,
-    "15-16": 0.05,
-    "16-17": 0.05,
-    "17-18": 0.05,
-    "18-19": 0.04,
-    "19-20": 0.035,
-    "20-21": 0.03,
-    "21-22": 0.025,
-    "22-23": 0.02,
-    "23-24": 0.02,
-}
-
+# Create a list that holds all the dry weather flow percentages (factors). Source: Module C2100 Leidraad Riolering
 dwfFactors = [
     [0, 0.015],
     [1, 0.015],
@@ -91,7 +56,7 @@ dwfFactors = [
     [14, 0.06],
     [15, 0.05],
     [16, 0.05],
-    [17, 0.06],
+    [17, 0.05],
     [18, 0.04],
     [19, 0.035],
     [20, 0.03],
