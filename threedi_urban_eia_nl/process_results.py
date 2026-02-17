500ff129f58970b126f1c16875e1bd922b9d7096
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2026-02-17 12:51:51 +0100

    Revert "More flexible rain file naming requirements + documentation"
    This reverts commit 401f7607bb0cd5933971cecd45f7875dc292ca3f.

--- threedi_urban_eia_nl/process_results.py
+++ threedi_urban_eia_nl/process_results.py
@@ -173,7 +173,7 @@ def download_results(
     while len(remaining) > 0:
         for simulation in remaining:
             simulation_id: int = simulation[0]
-            simulation_name: str = simulation[1]
+            isahw: str = simulation[1].split("isahw")[1]
             printProgressBar(total - len(remaining), total, "Downloading result files")
             status: SimulationStatus = api_call(
                 api.simulations_status_list, simulation_id
@@ -213,7 +213,7 @@ def download_results(
 
                     if debug and result.filename.startswith("log"):
                         "Download log files and unzip"
-                        sim_dir = simulations_dir / f"{simulation_id}-{simulation_name}"
+                        sim_dir = simulations_dir / f"{simulation_id}-isahw{isahw}"
                         sim_dir.mkdir(parents=True)
                         download = api_call(
                             api.simulations_results_files_download,
