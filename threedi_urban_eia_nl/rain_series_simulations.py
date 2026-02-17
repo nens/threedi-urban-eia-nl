0383e64c5fb2e1d8459b9ac3235921336698461f
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2026-02-17 10:48:16 +0100

    Lint

--- threedi_urban_eia_nl/rain_series_simulations.py
+++ threedi_urban_eia_nl/rain_series_simulations.py
@@ -60,10 +60,14 @@ def set_correct_aggregation_settings(api: V3BetaApi, simulation: Simulation):
     """Set aggregation settings to those required for urban environmental impact assessment"""
 
     # delete existing settings
-    current_settings = api.simulations_settings_aggregation_list(simulation.id, limit=99).results
+    current_settings = api.simulations_settings_aggregation_list(
+        simulation.id, limit=99
+    ).results
     for entry in current_settings:
         entry_id = entry.url.split("/")[-2]
-        api.simulations_settings_aggregation_delete(id=entry_id, simulation_pk=simulation.id)
+        api.simulations_settings_aggregation_delete(
+            id=entry_id, simulation_pk=simulation.id
+        )
 
     # post the new settings
     aggregation_settings = [
@@ -71,7 +75,8 @@ def set_correct_aggregation_settings(api: V3BetaApi, simulation: Simulation):
             "flow_variable": "discharge",
             "method": method,
             "interval": 3600,
-        } for method in REQUIRED_AGGREGATION_METHODS
+        }
+        for method in REQUIRED_AGGREGATION_METHODS
     ]
     for aggregation_setting in aggregation_settings:
         api.simulations_settings_aggregation_create(simulation.id, data=aggregation_setting)
